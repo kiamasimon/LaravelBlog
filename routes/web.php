5db1502c22fa9fc8381f512a4f2fd918cfc7e343
KIAMA <kiamasimon90@gmail.com>
2019-03-06 06:45:41 -0800

    Completed Tutorial 9 and 10

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,14 @@ Route::get('/', function () {
     return view('posts.index');
 });
 
+Route::get('/', 'PostController@getIndex')->name('index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/author/posts', 'HomeController@getPostForm')->name('post.form');
 Route::post('/author/posts', 'HomeController@createPost')->name('post.form');
 Route::get('/author/posts/detail/{id}', 'HomeController@getPost')->name('post.detail');
+Route::get('/author/posts/edit/{id}', 'HomeController@editPost')->name('post.edit');
+Route::post('/author/posts/edit/{id}', 'HomeController@updatePost')->name('post.update');
+Route::get('/author/posts/delete/{id}', 'HomeController@deletePost')->name('post.delete');
+Route::get('/posts/read/{post_id}', 'PostController@getFullPost')->name('post.read');
